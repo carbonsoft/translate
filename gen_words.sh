89bfd9704060cd5cb569df2a4bcfdf5745b0043b
Anton Klinskikh <a.klinskih@carbonsoft.ru>
2020-03-17 12:09:24 +0500

    Исправлено(Перевод): strongbash

--- gen_words.sh
+++ gen_words.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# echo "$0 $@ [$$] START" >&2
 . /app/base/usr/bin/activate
+echo Info: генерируем .po файлы для перевода
+echo Usage:  django_project_dir
+echo Example:  /app/asr_billing/usr/local/www/sites
 
 cd $1
-python2.6 /app/base/usr/lib/python2.6/site-packages/django/bin/django-admin.py makemessages -l en -a \
-        --no-wrap --no-location
+python2.6 /app/base/usr/lib/python2.6/site-packages/django/bin/django-admin.py \
+	makemessages -l en -a --no-wrap --no-location
 echo  $1/locale/en/LC_MESSAGES/django.po
+# echo "$0 $@ [$$] SUCCESS"
 exit 0
